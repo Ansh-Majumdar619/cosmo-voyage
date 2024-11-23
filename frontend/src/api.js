65bc4a51c1f527d39fa4387d0cf7b59a5e2c5657
Ansh Majumdar <137042201+Ansh-Majumdar619@users.noreply.github.com>
2024-11-23 16:24:35 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your backend URL if hosted elsewhere
+  baseURL: "https://cosmo-voyage-backend.onrender.com", // Replace with your backend URL if hosted elsewhere
 });
 
 export default API;
